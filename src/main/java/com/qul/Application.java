1db1d1363e39b463f98b842a7f51958b323481f1
qiuxia <qiuxia123>
2019-06-13 10:27:19 +0800

    修订版1.0.1

---
+++ src/main/java/com/qul/Application.java
@@ -0,0 +1,15 @@
+package com.qul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yanfazhongxin on 2019/6/12.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
